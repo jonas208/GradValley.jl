93ae376bcea20b26327d619d8537bdbab1f1265c
Jonas S <74866941+jonas208@users.noreply.github.com>
2023-01-15 15:55:11 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -12,6 +12,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/jonas208/GradValley.jl.git"
+    repo = "github.com/jonas208/GradValley.jl.git",
     devurl=""
 )
