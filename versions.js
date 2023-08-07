2009aea28988c91b496bcaff02f5e466ff8d365b
Jonas S <74866941+jonas208@users.noreply.github.com>
2023-08-07 18:07:30 +0200

    Add files via upload

--- versions.js
+++ versions.js
@@ -1,13 +1,5 @@
 var DOC_VERSIONS = [
-    "stable",
-    "v0.12",
-    "v0.11",
-    "v0.10",
-    "v0.9",
-    "v0.8",
-    "v0.7",
-    "v0.6",
     "dev",
   ];
-  var DOCUMENTER_NEWEST = "v0.12.165";
-  var DOCUMENTER_STABLE = "stable";
+  var DOCUMENTER_NEWEST = "dev";
+  var DOCUMENTER_STABLE = "dev";
