1af2aa291d998ab2fb6389ca94b8eafd61033b77
Jonas S <74866941+jonas208@users.noreply.github.com>
2023-08-23 21:19:38 +0200

    Add files via upload

--- src/optimization/gv_optimizers.jl
+++ src/optimization/gv_optimizers.jl
@@ -94,7 +94,7 @@ julia> model = SequentialContainer([Fc(1000, 500), Fc(500, 250), Fc(250, 125)])
 # initialize a MSGD optimizer with learning-rate equal 0.1 and momentum equal to 0.75 (otherwise default values)
 julia> optimizer = Nesterov(model, 0.1, momentum=0.75)
 # create some random input data
-julia> input = rand(Float32, 32, 1000)
+julia> input = rand(Float32, 1000, 32)
 # compute the output of the model
 julia> output = forward(model, input)
 # generate some random target values 
@@ -321,7 +321,7 @@ julia> model = SequentialContainer([Fc(1000, 500), Fc(500, 250), Fc(250, 125)])
 # initialize a Adam optimizer with default arguments
 julia> optimizer = Adam(model)
 # create some random input data
-julia> input = rand(Float32, 32, 1000)
+julia> input = rand(Float32, 1000, 32)
 # compute the output of the model
 julia> output = forward(model, input)
 # generate some random target values 
